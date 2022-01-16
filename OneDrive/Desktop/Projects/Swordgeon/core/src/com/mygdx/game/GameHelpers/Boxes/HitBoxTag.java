6eb7ab3c14ea4f8517a8806731a2c900a1811698
ZackZimny <72897309+ZackZimny@users.noreply.github.com>
2022-01-15 20:30:01 -0600

    Delete OneDrive/Desktop/Projects/Swordgeon directory

--- OneDrive/Desktop/Projects/Swordgeon/core/src/com/mygdx/game/GameHelpers/Boxes/HitBoxTag.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game.GameHelpers.Boxes;
-
-/**
- * Tags all the hit boxes in the game, so that when different hit boxes interact, they can cause different effects to occur
- */
-public enum HitBoxTag {
-    PLAYER_SWORD,
-    DEFAULT
-}
